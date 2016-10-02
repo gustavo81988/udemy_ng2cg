e1c489e0b6c3cc896e136707f24aa75275be6703
Gustavo Ramírez <gustavo81988@gmail.com>
2016-10-02 11:43:51 -0400

    Element Interaction with HostListener & HostBinding

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,12 +1,26 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
 export class HighlightDirective {
 
-  constructor(private elementRef: ElementRef, private renderer:Renderer) {
-    this.renderer.setElementStyle(this.elementRef.nativeElement,'background-color','green') 
+  @HostListener('mouseenter') mouseover(){
+    this.backgroundColor= "green";
+  };
+
+  @HostListener('mouseleave') mouseleave(){
+    this.backgroundColor= "white";
+  };
+
+  @HostBinding('style.backgroundColor') get setColor(){
+    return this.backgroundColor;
+  };
+
+  private backgroundColor = 'white';
+
+  constructor() {
+    //this.renderer.setElementStyle(this.elementRef.nativeElement,'background-color','green') 
   }
 
 }
