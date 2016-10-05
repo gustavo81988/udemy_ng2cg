1868a210260e0a45cdc35f3d4b9e61299bfda919
Gustavo Ramírez <gustavo81988@gmail.com>
2016-10-05 09:27:02 -0400

    Injecting Services into Services

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,9 +1,17 @@
+import { LogService } from './log.service';
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
 export class DataService {
 
   private data: string[] = [];
 
+  constructor (private logService: LogService){}
+
   public addData(input: string): void{
     this.data.push(input);
+    this.logService.writeToLog('Saved item: ' + input);
   }
 
   public getData(): string[]{
